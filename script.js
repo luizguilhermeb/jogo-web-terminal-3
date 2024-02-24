d05e3f33f72a6becf8892f885253562140c0aa66
luizguilhermeb <83599309+luizguilhermeb@users.noreply.github.com>
2024-02-24 18:08:05 -0300

    melhorias e eliminações
    sons da home tirados por falta de compatibilidade com reprodução automática, os sons serão focados durante o jogo e não na home.
    design da home melhorado significativamente.

--- script.js
+++ script.js
@@ -2,20 +2,15 @@ const gameTitleText = "Terminal3";
 const gameTitle = document.querySelector("#game-title");
 let counter = 0;
 
-let audio = new Audio("sounds/strong-digit.mp3");
-
 function typeWriter() {
   if (counter < gameTitleText.length) {
-    audio.pause();
-    audio.currentTime = 0;
     gameTitle.textContent += gameTitleText.charAt(counter); // Adiciona a caractere refereniada de gameTitleText
-    audio.play();
     if (gameTitleText.charAt(counter) == "3") {
       // Aplica estilo à caractere '3'
       gameTitle.innerHTML = "Terminal <span>3</span>";
     }
     counter++;
-    setTimeout(typeWriter, 1200); // Ajuste o tempo para controlar a velocidade de digitação
+    setTimeout(typeWriter, 500); // Ajuste o tempo para controlar a velocidade de digitação
   }
 }
 
