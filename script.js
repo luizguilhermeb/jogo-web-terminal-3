86b897294c83577721c080002df63aff40d76c7e
luizguilhermeb <83599309+luizguilhermeb@users.noreply.github.com>
2024-02-29 16:44:07 -0300

    página de créditos criada

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// === texto-animation from home ===
+
 const gameTitleText = "Terminal3";
 const gameTitle = document.querySelector("#game-title");
 let counter = 0;
@@ -14,4 +16,24 @@ function typeWriter() {
   }
 }
 
-setTimeout(typeWriter, 2000);
+setTimeout(typeWriter, 1500);
+
+// === Functions from credits page ===
+
+credits = document.querySelector('#credits')
+creditsLink = document.querySelector('#credits-link')
+buttonBack = document.querySelector('#back-button')
+
+function openCredits() {
+  credits.style.visibility = 'visible';
+  credits.style.opacity = '1';
+}
+creditsLink.addEventListener('click', openCredits)
+
+function closeCredits() {
+  credits.style.opacity = '0';
+  setTimeout(function() {
+    credits.style.visibility = 'hidden';
+  }, 700);
+}
+buttonBack.addEventListener('click', closeCredits)
