cfd961ba4b0dceee8834fbb0e503e98af95ee56d
luizguilhermeb <83599309+luizguilhermeb@users.noreply.github.com>
2024-09-10 14:39:38 -0400

    save

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function changeSoundtrack() {
   soundtrackPlayer.play();
 
   if (soundCounter == 2) {
-    soundtrackPlayer.volume = 0.3;
+    soundtrackPlayer.volume = 0.2;
   } else {
-    soundtrackPlayer.volume = 0.4;
+    soundtrackPlayer.volume = 0.3;
   }
 }
 
@@ -52,6 +52,7 @@ function changeSoundState() {
     soundButton.setAttribute("aria-label", "Ativar trilha sonora");
   } else {
     soundButtonPath.setAttribute("d", svgOnSound);
+    soundtrackPlayer.volume = 0.3;
     soundtrackPlayer.play();
     soundButton.setAttribute("aria-label", "Desativar trilha sonora");
   }
@@ -64,17 +65,27 @@ soundButton.addEventListener("click", changeSoundState);
 const gameTitleText = "Terminal3";
 const gameTitleElement = document.querySelector("#game-title");
 let counter = 0;
+let digitTime = 0
 
 function typeWriter() {
   if (counter < gameTitleText.length) {
     gameTitleElement.textContent += gameTitleText.charAt(counter); // Adiciona a caractere refereniada de gameTitleText
-    clickSoundFunction()
+    clickSoundFunction();
     if (gameTitleText.charAt(counter) == "3") {
       // Aplica estilo à caractere '3'
       gameTitleElement.innerHTML = "Terminal <span>3</span>";
     }
     counter++;
-    setTimeout(typeWriter, Math.random() * 1500); // Ajuste o tempo para controlar a velocidade de digitação
+    function randomDigitGenerator() {
+      digitTime = Math.random() * 1500 // Ajuste o tempo para controlar a velocidade de digitação
+      if (digitTime < 500) {
+        randomDigitGenerator()
+      }
+      else {
+        setTimeout(typeWriter, digitTime);
+      }
+    }
+    randomDigitGenerator()
   }
 }
 
@@ -84,7 +95,7 @@ const soudtrackSection = document.querySelector("#soundtrack-section");
 
 function closeSoundtrackSection() {
   soudtrackSection.style.opacity = "0";
-  soudtrackSection.style.visibility = "hidden"
+  soudtrackSection.style.visibility = "hidden";
 }
 
 function enableLinksHome() {
@@ -109,7 +120,7 @@ soundtrackOptionOn.addEventListener("click", function () {
     "M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM412.6 181.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5z"
   );
   soundtrackPlayer.play();
-  soundtrackPlayer.volume = 0.5;
+  soundtrackPlayer.volume = 0.3;
   soundButton.setAttribute("aria-label", "Desativar trilha sonora");
   header.style.visibility = "visible";
   aside.style.visibility = "visible";
@@ -159,9 +170,11 @@ soundtrackOptionOff.addEventListener("click", function () {
 
 // click sound for click in link menu
 linkMenu.forEach((linkMenu) => {
-  linkMenu.addEventListener("click", function () {
-    clickSoundFunction();
-  });
+  if (linkMenu.id != "play-link") {
+    linkMenu.addEventListener("click", function () {
+      clickSoundFunction();
+    });
+  }
 });
 
 backButton = document.querySelectorAll(".back-button");
@@ -194,7 +207,7 @@ function closeCredits() {
   });
   header.setAttribute("aria-hidden", "false");
   credits.style.opacity = "0";
-  credits.style.visibility = "hidden"
+  credits.style.visibility = "hidden";
 }
 buttonBackCredits.addEventListener("click", closeCredits);
 
@@ -276,23 +289,23 @@ function typeRandomCharacters() {
 const playLink = document.querySelector("#play-link");
 const initialGamePage = document.querySelector("#initial-game-page");
 const gamePage = document.querySelector("#game-page");
-const spatialSound = new Audio("sounds/sound-effects/spatial-click.mp3")
+const spatialSound = new Audio("sounds/sound-effects/spatial-click.mp3");
 
 playLink.addEventListener("click", function () {
-  spatialSound.play()
-  header.style.opacity = '0'
-  header.style.visibility = 'hidden'
-  setTimeout(function() {
-    initialGamePage.style.visibility = 'visible'
-    initialGamePage.style.opacity = '1'
-    setTimeout(typeRandomCharacters, 500)
-  }, 1500)
+  spatialSound.play();
+  header.style.opacity = "0";
+  header.style.visibility = "hidden";
+  setTimeout(function () {
+    initialGamePage.style.visibility = "visible";
+    initialGamePage.style.opacity = "1";
+    setTimeout(typeRandomCharacters, 500);
+  }, 1500);
   setTimeout(function () {
     initialGamePage.style.opacity = "0";
-    initialGamePage.style.visibility = "hidden"
-    gamePage.style.visibility = "visible"
-    setTimeout(function() {
-      gamePage.style.opacity = "1"
-    }, 1500)
+    initialGamePage.style.visibility = "hidden";
+    gamePage.style.visibility = "visible";
+    setTimeout(function () {
+      gamePage.style.opacity = "1";
+    }, 1500);
   }, 11000);
 });
